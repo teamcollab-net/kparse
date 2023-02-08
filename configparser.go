c849835a6561ac5ab5dca2fca794ec3588c78978
Vinícius Garcia <vingarcia00@gmail.com>
2023-02-08 10:44:27 -0300

    Add some public functions for parsing yaml configs

--- configparser.go
+++ configparser.go
@@ -1,1 +1,58 @@
 package configparser
+
+import (
+	"bytes"
+	"io"
+	"os"
+
+	"github.com/vingarcia/structscanner"
+	"gopkg.in/yaml.v2"
+)
+
+func MustParseYAMLFile(targetStruct interface{}, filename string) {
+	err := ParseYAMLFile(targetStruct, filename)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ParseYAMLFile(targetStruct interface{}, filename string) error {
+	file, err := os.Open(filename)
+	if err != nil {
+		return err
+	}
+
+	defer file.Close()
+	return ParseYAMLReader(targetStruct, file)
+}
+
+func MustParseYAML(targetStruct interface{}, file []byte) {
+	err := ParseYAML(targetStruct, file)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ParseYAML(targetStruct interface{}, file []byte) error {
+	return ParseYAMLReader(targetStruct, bytes.NewReader(file))
+}
+
+func MustParseYAMLReader(targetStruct interface{}, file io.Reader) {
+	err := ParseYAMLReader(targetStruct, file)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func ParseYAMLReader(targetStruct interface{}, file io.Reader) error {
+	var parsedYaml map[string]interface{}
+	err := yaml.NewDecoder(file).Decode(&parsedYaml)
+	if err != nil {
+		return err
+	}
+
+	return structscanner.Decode(
+		targetStruct,
+		newMapTagDecoder("yaml", parsedYaml),
+	)
+}
